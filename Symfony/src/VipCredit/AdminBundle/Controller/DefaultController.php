7e642ed638ef0d7fedfb3af388aed18df0731af7
79163518 <79163518@qq.com>
2013-06-01 10:48:28 +0800

    6.1

--- Symfony/src/VipCredit/AdminBundle/Controller/DefaultController.php
+++ Symfony/src/VipCredit/AdminBundle/Controller/DefaultController.php
@@ -3,11 +3,51 @@
 namespace VipCredit\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('VipCreditAdminBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    public function loginAction()
+    {
+        if ($this->get('request')->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $this->get('request')->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $this->get('request')->getSession()->get(SecurityContext::AUTHENTICATION_ERROR);
+        }
+        //$request = $this->getRequest();
+        //$session = $request->getSession();
+
+        //// get the login error if there is one
+        //if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            //$error = $request->attributes->get(
+                //SecurityContext::AUTHENTICATION_ERROR
+            //);
+        //} else {
+            //var_dump($error->getMessage());exit;
+            //$error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            //$session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        //}
+
+        return $this->render(
+            'VipCreditAdminBundle:Default:login.html.twig',
+            array(
+                'last_username' => $this->get('request')->getSession()->get(SecurityContext::LAST_USERNAME),
+                'error'         => $error,
+                'page_charset'  => 'UTF-8',
+                'page_title'    => 'Vip Credit',
+                'js_root'       => 'http://www.phpwind.test/res/dev',
+                'js_version'    => '0.0.1',
+                'js_ajax_token' => 'ddddddddd',
+            )
+        );
+    }
+
+    public function logoutAction()
+    {
     }
 }
--- Symfony/src/VipCredit/AdminBundle/Resources/config/routing.php
+++ Symfony/src/VipCredit/AdminBundle/Resources/config/routing.php
@@ -6,6 +6,17 @@ use Symfony\Component\Routing\Route;
 $collection = new RouteCollection();
 $collection->add('vip_credit_admin_homepage', new Route('/hello/{name}', array(
     '_controller' => 'VipCreditAdminBundle:Default:index',
+    'name' => 'tom',
+)));
+
+$collection->add('vip_credit_admin_login', new Route('/login', array(
+    '_controller' => 'VipCreditAdminBundle:Default:login',
+)));
+
+$collection->add('vip_credit_admin_login_check', new Route('/loginCheck'));
+
+$collection->add('vip_credit_admin_logout', new Route('/logout', array(
+    '_controller' => 'VipCreditAdminBundle:Default:logout',
 )));
 
 return $collection;
